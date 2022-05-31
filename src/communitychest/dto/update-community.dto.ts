33e179c2a086c58ce4bd27662708e1da64d3fe9e
Neeraj <neeraj.indore@gmail.com>
2022-05-31 13:50:34 +0530

    fix double issue

--- src/communitychest/dto/update-community.dto.ts
+++ src/communitychest/dto/update-community.dto.ts
@@ -1,5 +1,4 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { Double } from 'typeorm';
 
 export class UpdateCommunityDto {
   @ApiProperty({ required: false })
@@ -21,20 +20,20 @@ export class UpdateCommunityDto {
   slot5?: number;
 
   @ApiProperty({ required: false })
-  totalStr?: Double;
+  totalStr?: string;
 
   @ApiProperty({ required: false })
-  totalDex?: Double;
+  totalDex?: string;
 
   @ApiProperty({ required: false })
-  totalLuk?: Double;
+  totalLuk?: string;
 
   @ApiProperty({ required: false })
-  totalPrep?: Double;
+  totalPrep?: string;
 
   @ApiProperty({ required: false })
-  totalHp?: Double;
+  totalHp?: string;
 
   @ApiProperty({ required: false })
-  totalMp?: Double;
+  totalMp?: string;
 }
