03d7cecd78da343432d331c7e743afac354f6d06
Neeraj <neeraj.indore@gmail.com>
2022-05-31 12:12:44 +0530

    fix issue

--- src/entergame/entergame.module.ts
+++ src/entergame/entergame.module.ts
@@ -3,12 +3,12 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 
 import { EnterGame } from './entergame.entity';
-import { UsersProfileController } from './entergame.controller';
+import { EnterGameController } from './entergame.controller';
 import { EnterGameService } from './entergame.service';
 
 @Module({
   imports: [TypeOrmModule.forFeature([EnterGame]), ConfigModule],
   providers: [EnterGameService, ConfigService],
-  controllers: [UsersProfileController],
+  controllers: [EnterGameController],
 })
 export class EnterGameModule {}
