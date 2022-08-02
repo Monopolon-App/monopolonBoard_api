ae1ea55b717130f9a590a8eccd02119e3f553219
hardikbeladiya <beladiyahardik7@gmail.com>
2022-08-02 16:46:38 +0530

    change urls for testnet as well as mainnet.

--- src/constants/constants.ts
+++ src/constants/constants.ts
@@ -15,13 +15,12 @@ export const MGM_CONTRACT_ADDRESS_FOR_NEW_COMPANY_ADDRESS = {
 
 export const WS_PROVIDER_URL = {
   TESTNET:
-    'wss://ws-nd-731-442-828.p2pify.com/939137935cdae3b5e1c59d530a1a1dab',
+    'wss://ws-nd-598-068-522.p2pify.com/787c188bc7cf0bf9797204b6b11ec0cc',
   MAINNET:
     'wss://ws-nd-731-442-828.p2pify.com/939137935cdae3b5e1c59d530a1a1dab',
 };
 
 export const RPC_PROVIDER_URL = {
-  // MAINNET: 'https://nd-731-442-828.p2pify.com/939137935cdae3b5e1c59d530a1a1dab',
-  MAINNET: 'https://bsc-dataseed1.binance.org/',
-  TESTNET: 'https://nd-731-442-828.p2pify.com/939137935cdae3b5e1c59d530a1a1dab',
+  MAINNET: 'https://nd-731-442-828.p2pify.com/939137935cdae3b5e1c59d530a1a1dab',
+  TESTNET: 'https://nd-598-068-522.p2pify.com/787c188bc7cf0bf9797204b6b11ec0cc',
 };
