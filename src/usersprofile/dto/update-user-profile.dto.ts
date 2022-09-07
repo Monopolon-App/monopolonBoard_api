8df247e8ddc2525198c21ac77302e67c34a50f88
hardikbeladiya <beladiyahardik7@gmail.com>
2022-09-07 13:14:16 +0530

    change in update userProfile api.

--- src/usersprofile/dto/update-user-profile.dto.ts
+++ src/usersprofile/dto/update-user-profile.dto.ts
@@ -29,6 +29,9 @@ export class UpdateUserDto {
   mlonRewardsAccumulated?: string;
 
   @ApiProperty({ required: false })
+  lastMinedMLON?: number;
+
+  @ApiProperty({ required: false })
   lastRollTimeStamp?: Date;
 
   @ApiProperty({ required: false })
